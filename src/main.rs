55e3ff6183725044cc6ad426b584e47d3f87f4ac
Benoit Senechal <benoit8senechal@gmail.com>
2022-11-18 18:07:19 +0100

    Add byte count functionality

--- src/main.rs
+++ src/main.rs
@@ -16,18 +16,22 @@ struct Args {
     /// Output file
     #[arg(short)]
     output: String,
+
+    /// Number of bytes to copy
+    #[arg(long, default_value_t = 512)]
+    count: usize,
 }
 
 fn main() -> Result<(), anyhow::Error> {
     let args = Args::parse();
 
     let input_file = File::open(args.input).expect("Failed to open input file");
-    let mut buf_reader = BufReader::new(input_file);
-    let mut contents = String::new();
-    buf_reader.read_to_string(&mut contents)?;
+    let mut buf_reader = BufReader::with_capacity(args.count, input_file);
+    let mut contents = vec![0; args.count];
+    let byte_read = buf_reader.read(&mut contents)?;
 
     let mut output_file = File::create(args.output).expect("Failed to open output file");
-    output_file.write_all(contents.as_bytes())?;
+    output_file.write_all(&contents[..byte_read])?;
     output_file.sync_all()?;
     Ok(())
 }
